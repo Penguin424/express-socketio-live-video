88a088f7c5aedf93b591b2e0f3a4491c5a8ed082
Pablo Rizo <prozasoconw4@gmail.com>
2020-10-19 22:23:41 -0500

    sin get

--- src/routes/littlezoom.routes.js
+++ src/routes/littlezoom.routes.js
@@ -1,19 +1,19 @@
 const { Router } = require('express');
 const router = Router();
 
-router.get('/', async(req, res) => 
-{
-    try 
-    {
-        res.redirect('index.html');
-    } 
-    catch (error) 
-    {
-        res.status(400).json({
-            ok: false,
-            error
-        })    
-    }
-});
+// router.get('/', async(req, res) => 
+// {
+//     try 
+//     {
+//         res.redirect('index.html');
+//     } 
+//     catch (error) 
+//     {
+//         res.status(400).json({
+//             ok: false,
+//             error
+//         })    
+//     }
+// });
 
 module.exports = router;
